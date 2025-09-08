c1ef245fa70ff6f61b9a00f082332bfa2d1c9e43
Soham Chousalkar <sohamchousalkar123@gmail.com>
2025-09-07 20:21:54 -0400

    Fix Vite config: let Vite handle asset paths automatically

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 }) 
